6a62a3a7a4e0c560beba25e0ec5a840e03ff422a
David Lima <127369649+Davidlfprojects@users.noreply.github.com>
2023-03-09 00:54:12 -0300

    Commit inical

---
+++ teste.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+cout << "Teste do programa ENIAC\n";
+ //TIPO NOME;
+ //TIPO NOME = VALOR;
+ 
+int vidas=0; //10, 25
+string nome2=""; //""
+double decimal=5.2; //2.4999999
+float decimal2=5.2; //2.5
+bool vivo=true; //true=verdadeiro / false=falso
+string nome=""; //""
+
+cout << "Digite sua idade: ";
+cin >> vidas;
+cout << "Digite seu mes de nascimento: ";
+cin >> nome2;
+cout <<"Digite seu ano de nascimento: ";
+cin >> decimal;
+cout << "Digite o seu nome: ";
+cin >> nome;
+
+
+cout <<"\nIdade : " << vidas << "\nMes: " << nome2 << "\nNasceu no ano: " << decimal << "\nchances de sair vivo do projeto ENIAC: " << vivo << "\nNome: " << nome << "\n";
+
+cout << "Apos preencher isso desligue seu dispositivo em 10segundos ou o mesmo explodira :)\n";
+             system ("pause");
+ return 0;
+
+}
